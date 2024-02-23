fad6d49bdd1600f786a457abdda3390ec9456de5
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2024-02-23 02:34:58 +0100

    x

--- popup.js
+++ popup.js
@@ -1,5 +1,1 @@
-document.getElementById('showOverlay').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "showOverlay"});
-    });
-});
+console.log('Hello world from popup.js!');
